a2085a234211e4c8f045e3c9ac5032ff65d6b20c
namsoo5 <are34@naver.com>
2021-09-26 18:04:47 +0900

    :bulb: 네트워크 통신 구조설계

---
+++ HikingClub/Network/API.swift
@@ -0,0 +1,53 @@
+//
+//  API.swift
+//  HikingClub
+//
+//  Created by 남수김 on 2021/09/25.
+//
+
+import Foundation
+import Moya
+
+// MARK: - API정의
+enum API {
+    /// 테스트경로
+    case mock
+}
+
+extension API: TargetType {
+    // MARK: - 경로정의
+    var path: String {
+        switch self {
+        case .mock:
+            return ""
+        }
+    }
+    
+    // MARK: - HTTP형식 정의
+    var method: Moya.Method {
+        switch self {
+        case .mock:
+            return .get
+        }
+    }
+    
+    // MARK: - 데이터정의
+    var task: Task {
+        switch self {
+        case .mock:
+            return .requestPlain
+        }
+    }
+}
+
+// MARK: - 디폴트 값
+extension TargetType {
+    var baseURL: URL {
+        URL(string: BaseAPI.baseAPI)!
+    }
+    
+    var headers: [String : String]? {
+        ["Content-Type": "application/json"]
+    }
+}
+
---
+++ HikingClub/Network/BaseAPI.swift
@@ -0,0 +1,12 @@
+//
+//  BaseAPI.swift
+//  HikingClub
+//
+//  Created by 남수김 on 2021/09/25.
+//
+
+import Foundation
+
+enum BaseAPI {
+    static let baseAPI = "https://www.google.com"
+}
