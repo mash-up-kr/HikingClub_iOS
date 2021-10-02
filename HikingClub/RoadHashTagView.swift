eb870269c48cc6427611de05d9c5ac358b8c64fe
32Moon <lmj970302@gmail.com>
2021-10-03 02:01:41 +0900

    :sparkles:

---
+++ HikingClub/RoadHashTagView.swift
@@ -0,0 +1,19 @@
+//
+//  RoadHashTagView.swift
+//  HikingClub
+//
+//  Created by 이문정 on 2021/10/03.
+//
+
+import UIKit
+import SnapKit
+
+class RoadHashTagView: UIView {
+    let hashTagLable = UILabel()
+    
+    // label 폰트
+    // View 색
+    // 레이아웃 잡기
+    func setText() {
+    }
+}
