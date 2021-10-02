88e34a23116e0c350642dd492d241e89affe0b0a
namsoo5 <are34@naver.com>
2021-10-03 01:52:40 +0900

    :bulb: 헤더뷰 기본UI구현

--- HikingClub/HitThemeTableHeaderView.swift
+++ HikingClub/HitThemeTableHeaderView.swift
@@ -13,6 +13,14 @@ final class HitThemeTableHeaderView: UITableViewHeaderFooterView {
     @IBOutlet private weak var currentLocationButton: UIButton!
     
     override func awakeFromNib() {
-        
+        super.awakeFromNib()
+        configureUI()
+    }
+    
+    func configureUI() {
+        hitThemeLabel.setFont(.semiBold16)
+        currentLocationButton.semanticContentAttribute = .forceRightToLeft
+        currentLocationButton.setTitle("현위치", for: .normal)
+        currentLocationButton.setImage(UIImage(named: "downArrow"), for: .normal)
     }
 }
