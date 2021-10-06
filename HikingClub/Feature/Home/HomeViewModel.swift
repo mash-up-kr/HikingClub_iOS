72ec1153ba0b31b4645055a04d04d313ea0ac5b8
AhnSangHoon <ansh0324@gmail.com>
2021-10-06 22:44:09 +0900

    :bulb: HomeViewModel 생성 및 HomeViewController 변경

---
+++ HikingClub/Feature/Home/HomeViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewModel.swift
+//  HikingClub
+//
+//  Created by AhnSangHoon on 2021/10/06.
+//
+
+import UIKit
+
+final class HomeViewModel: BaseViewModel {
+    var sampleTags: [String] {
+        ["망리단길","메롱길","단풍길","단풍길1","단풍길2","단풍길3"]
+    }
+}
