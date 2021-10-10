7e2b39e7eeb2e159b0ae8a8fee4110c948ca1cdb
AhnSangHoon <ansh0324@gmail.com>
2021-10-10 13:56:25 +0900

    :bulb: SignUpViewController 뷰 완성

--- HikingClub/Feature/Login/SignUp/SignUpViewController.swift
+++ HikingClub/Feature/Login/SignUp/SignUpViewController.swift
@@ -7,9 +7,30 @@
 
 import UIKit
 
-class SignUpViewController: BaseViewController<BaseViewModel> {
-    private let greetingView: GreetingView = GreetingView()
-    private let termStackView: SignUpTermsStackView = SignUpTermsStackView()
+final class SignUpViewController: BaseViewController<BaseViewModel> {
+    private let navigationArea: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        return view
+    }()
+    private let greetingView = GreetingView()
+    private let termStackView = SignUpTermsStackView()
+    private let termNoticeLabel: UILabel = {
+        let label = UILabel()
+        label.setFont(.medium12)
+        label.numberOfLines = .zero
+        label.textColor = .lightGray
+        label.lineBreakMode = .byCharWrapping
+        label.text = "개인정보 처리 방침에 동의하지 않으셔도 비회원으로 서비스 이용이 가능합니다. 비회원으로 서비스 이용시 일부 기능에 제한이 있을 수 있습니다."
+        return label
+    }()
+    // TODO: CAT Button Component로 교쳬 예정
+    private let agreeButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("동의하기", for: .normal)
+        button.backgroundColor = .gray
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,15 +44,44 @@ class SignUpViewController: BaseViewController<BaseViewModel> {
     
     // MARK: - Layout
     private func layout() {
-        view.addSubviews(greetingView, termStackView)
+        view.addSubviews(navigationArea, greetingView, termStackView, termNoticeLabel, agreeButton)
+        navigationArea.snp.makeConstraints {
+            $0.top.equalTo(view)
+            $0.leading.trailing.equalToSuperview()
+            
+            $0.height.equalTo(98)
+        }
         greetingView.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(50)
+            $0.top.equalTo(navigationArea.snp.bottom).offset(48)
             $0.leading.trailing.equalToSuperview()
         }
-        
         termStackView.snp.makeConstraints {
             $0.top.equalTo(greetingView.snp.bottom).offset(50)
             $0.leading.trailing.equalToSuperview()
         }
+        termNoticeLabel.snp.makeConstraints {
+            $0.top.equalTo(termStackView.snp.bottom).offset(4)
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().inset(16)
+        }
+        agreeButton.snp.makeConstraints {
+            $0.bottom.equalTo(view)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(122)
+        }
+    }
+    
+    // MARK: - Bind
+    
+    private func bind() {
+        agreeButton.rx.tap
+            .subscribe(onNext: { [weak self] _ in
+                self?.navigateToSignUpInputViewController()
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func navigateToSignUpInputViewController() {
+        
     }
 }
