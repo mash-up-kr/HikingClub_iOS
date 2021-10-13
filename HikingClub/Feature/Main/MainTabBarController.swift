ebca4208097388f9cfa7e24d3c6cc1e2d15dda7e
AhnSangHoon <ansh0324@gmail.com>
2021-10-13 23:02:24 +0900

    :recycle: BaseViewController, BaseViewModel 수정

--- HikingClub/Feature/Main/MainTabBarController.swift
+++ HikingClub/Feature/Main/MainTabBarController.swift
@@ -8,9 +8,16 @@
 import UIKit
 
 final class MainTabBarController: UITabBarController {
+    private let homeViewController: UINavigationController = {
+        let homeStoryBoard = UIStoryboard(name: "Home", bundle: nil)
+        let homeViewController = homeStoryBoard.instantiateViewController(identifier: "HomeViewController") { coder -> HomeViewController in
+            return .init(coder, HomeViewModel())!
+        }
+        return homeViewController.wrappedByNavigationController()
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setViewControllers([homeViewController], animated: true)
     }
 }
