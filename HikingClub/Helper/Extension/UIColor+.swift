30d11d5c6e0bd6eea8f64c6935a990b8e550387a
namsoo5 <are34@naver.com>
2021-10-15 17:55:02 +0900

    :blub: 컬러추가

--- HikingClub/Helper/Extension/UIColor+.swift
+++ HikingClub/Helper/Extension/UIColor+.swift
@@ -9,10 +9,12 @@ import UIKit
 
 extension UIColor {
     static let green700 = UIColor(named: "green700")!
+    static let green500 = UIColor(named: "green500")!
     
     static let red500 = UIColor(named: "red500")!
     
     static let gray700 = UIColor(named: "gray700")!
+    static let gray500 = UIColor(named: "gray500")!
     static let gray100 = UIColor(named: "gray100")!
     
     var disable: UIColor {
