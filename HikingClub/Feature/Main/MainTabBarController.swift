4c1cd37514d4538f71f4253b2c7b88ba1b21b63a
AhnSangHoon <ansh0324@gmail.com>
2021-10-17 17:28:58 +0900

    :bulb: add tabBar item

--- HikingClub/Feature/Main/MainTabBarController.swift
+++ HikingClub/Feature/Main/MainTabBarController.swift
@@ -16,8 +16,19 @@ final class MainTabBarController: UITabBarController {
         return homeViewController.wrappedByNavigationController()
     }()
     
+    private let homeTabBarItem = UITabBarItem(title: "홈", image: nil, tag: 1)
+    
+    private let searchTabBarItem = UITabBarItem(title: "검색", image: nil, tag: 2)
+    
+    private let writeTabBarItem = UITabBarItem(title: "글등록", image: nil, tag: 3)
+    
+    private let mypageTabBarItem = UITabBarItem(title: "마이페이지", image: nil, tag: 4)
+    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        homeViewController.tabBarItem = homeTabBarItem
+        
         setViewControllers([homeViewController], animated: true)
     }
 }
