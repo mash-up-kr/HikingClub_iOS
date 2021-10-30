caad020552e6eeda06d0875277f0bf6a95113175
AhnSangHoon <ansh0324@gmail.com>
2021-10-31 00:15:37 +0900

    :sparkles: SignIn API

---
+++ HikingClub/Network/SignInService/Response/SignInResponseModel.swift
@@ -0,0 +1,12 @@
+//
+//  SignInResponseModel.swift
+//  HikingClub
+//
+//  Created by AhnSangHoon on 2021/10/31.
+//
+
+import Foundation
+
+struct SignInResponseModel: Decodable {
+    let accessToken: String
+}
