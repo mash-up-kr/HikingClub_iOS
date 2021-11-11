3fdbd89c68936d112689a83e29caa22560a3e128
namsoo5 <are34@naver.com>
2021-11-11 18:10:28 +0900

    :bulb: 헤더뷰 UI수정
    - 변경된 헤더뷰로 수정(상단텝 추가)

--- HikingClub/Feature/Home/HomeViewController.swift
+++ HikingClub/Feature/Home/HomeViewController.swift
@@ -12,12 +12,18 @@ import RxSwift
 final class HomeViewController: BaseViewController<HomeViewModel> {
     
     @IBOutlet weak var tableView: UITableView!
+    private lazy var tabbar: CategoryTabCollectionView = {
+        let tabbar = CategoryTabCollectionView()
+        tabbar.backgroundColor = .gray100
+        tabbar.rx.setDelegate(self).disposed(by: disposeBag)
+        return tabbar
+    }()
     
     override func attribute() {
         super.attribute()
         
-        tableView.register(RoadTableViewCell.self)
-        tableView.register(headerFooter: HitThemeTableHeaderView.self)
+        
+        setTableView()
     }
     
     override func bind() {
@@ -31,15 +37,54 @@ final class HomeViewController: BaseViewController<HomeViewModel> {
         // FIXME: - 목데이터 삭제
         viewModel.roadDatas.accept([["망리단길"],["문정이바보", "그만졸아"],["메롱길", "단풍길","11111","!11111"],[],["단풍길3","단풍길","단풍길"]])
     }
+    
+    func setTableView() {
+        tableView.register(RoadTableViewCell.self)
+        tableView.register(headerFooter: HitThemeTableHeaderView.self)
+    }
 }
 
 extension HomeViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: String(describing: HitThemeTableHeaderView.self))
-        return headerView
+        let containerView = UIView()
+        containerView.backgroundColor = .white
+        
+        Observable.just(["하나","둘","셋"])
+            .bind(to: tabbar.rx.items(cellIdentifier: CategoryTabCollectionView.cellIdentifier,
+                                              cellType: CategoryTabCollectionViewCell.self)) { item, cellModel, cell in
+                cell.configure(with: cellModel)
+            }.disposed(by: disposeBag)
+        
+        guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "HitThemeTableHeaderView") else {
+            return nil
+        }
+        headerView.backgroundColor = .white
+        
+        containerView.addSubViews(headerView, tabbar)
+        headerView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(174)
+        }
+        tabbar.snp.makeConstraints {
+            $0.top.equalTo(headerView.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(57)
+            $0.bottom.equalToSuperview()
+        }
+        
+        return containerView
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 223
+        return 231
+    }
+}
+
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let word = ["하나","둘","셋"][indexPath.item]
+        return tabbar.cellSize(text: word)
     }
 }
