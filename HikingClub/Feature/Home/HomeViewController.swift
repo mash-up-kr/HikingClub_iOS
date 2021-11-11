b7440abd322e6b2069e2afc5466797938f446ec3
namsoo5 <are34@naver.com>
2021-11-12 03:02:30 +0900

    :bulb: 상단 위치탭바 이벤트 구현

--- HikingClub/Feature/Home/HomeViewController.swift
+++ HikingClub/Feature/Home/HomeViewController.swift
@@ -12,12 +12,18 @@ import RxSwift
 final class HomeViewController: BaseViewController<HomeViewModel> {
     
     @IBOutlet weak var tableView: UITableView!
-    private lazy var tabbar: CategoryTabCollectionView = {
+    private lazy var locationTabbar: CategoryTabCollectionView = {
         let tabbar = CategoryTabCollectionView()
         tabbar.backgroundColor = .gray100
         tabbar.rx.setDelegate(self).disposed(by: disposeBag)
         return tabbar
     }()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // 초기선택설정
+        locationTabbar.selectItem(at: IndexPath(row: 0, section: 0), animated: true, scrollPosition: .left)
+    }
     
     override func attribute() {
         super.attribute()
@@ -38,10 +44,17 @@ final class HomeViewController: BaseViewController<HomeViewModel> {
         
         // 상단 탭바
         viewModel.locations
-            .bind(to: tabbar.rx.items(cellIdentifier: CategoryTabCollectionView.cellIdentifier,
+            .bind(to: locationTabbar.rx.items(cellIdentifier: CategoryTabCollectionView.cellIdentifier,
                                       cellType: CategoryTabCollectionViewCell.self)) { item, cellModel, cell in
                 cell.configure(with: cellModel)
             }.disposed(by: disposeBag)
+        
+        // TODO: - 선택시 통신
+        locationTabbar.rx.itemSelected
+            .subscribe(onNext: {
+                print($0)
+            })
+            .disposed(by: disposeBag)
         
         // FIXME: - 목데이터 삭제
         viewModel.mockData()
@@ -61,13 +74,13 @@ extension HomeViewController: UITableViewDelegate {
             return nil
         }
         
-        containerView.addSubViews(headerView, tabbar)
+        containerView.addSubViews(headerView, locationTabbar)
         headerView.snp.makeConstraints {
             $0.top.equalToSuperview()
             $0.leading.trailing.equalToSuperview()
             $0.height.equalTo(174)
         }
-        tabbar.snp.remakeConstraints {
+        locationTabbar.snp.remakeConstraints {
             $0.top.equalTo(headerView.snp.bottom)
             $0.leading.trailing.equalToSuperview()
             $0.height.equalTo(57)
@@ -86,6 +99,6 @@ extension HomeViewController: UITableViewDelegate {
 extension HomeViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let word = viewModel.locations.value[indexPath.item]
-        return tabbar.cellSize(text: word)
+        return locationTabbar.cellSize(text: word)
     }
 }
