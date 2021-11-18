258add18930267a02265b64e9c04337f0fce54eb
namsoo5 <are34@naver.com>
2021-11-18 13:41:30 +0900

    :bulb: 홈 길정보 리스트 응답모델

---
+++ HikingClub/Network/PlaceService/Response/RoadModel.swift
@@ -0,0 +1,31 @@
+//
+//  RoadModel.swift
+//  HikingClub
+//
+//  Created by 남수김 on 2021/11/18.
+//
+
+import Foundation
+
+struct RoadResponse {
+    let roads: [Road]
+}
+
+struct Road: Decodable {
+    let id: String
+    let title: String
+    let content: String
+    let distance: Int
+    let place: String
+    let category: CategoryIcon
+    let routes: [[Int]]
+    let spots: [Spot]
+    let images: [String]
+    let hashtags: [String]
+}
+
+struct Spot: Decodable {
+    let title: String
+    let content: String
+    let point: [Int]
+}
