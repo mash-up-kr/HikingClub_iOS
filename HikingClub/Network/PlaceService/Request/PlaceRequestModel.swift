d1faf9317483227d100daca398f7fdea407ee1fe
AhnSangHoon <ansh0324@gmail.com>
2021-11-25 23:07:04 +0900

    :bulb: 현재위치(lat, long) 기반 동네 검색 추가

--- HikingClub/Network/PlaceService/Request/PlaceRequestModel.swift
+++ HikingClub/Network/PlaceService/Request/PlaceRequestModel.swift
@@ -10,7 +10,25 @@ import UIKit
 enum PlaceRequestModel {
     struct SearchModel: Encodable {
         var keyword: String = ""
-        var pageSize: Int = 10
+        var pageSize: Int = 30
+        var long: Double?
+        var lat: Double?
+        
+        enum CodingKeys: String, CodingKey {
+            case keyword
+            case pageSize
+            case long = "position_y"
+            case lat = "position_x"
+        }
+        
+        init(lat: Double, long: Double) {
+            self.lat = lat
+            self.long = long
+        }
+        
+        init(keyword: String) {
+            self.keyword = keyword
+        }
     }
     
     struct MyRoadModel: Encodable {
