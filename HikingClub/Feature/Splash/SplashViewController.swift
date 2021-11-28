90ec1fb2206444483c495364c85134e66cc7ad4e
32Moon <lmj970302@gmail.com>
2021-11-28 16:54:33 +0900

    :bulb: 스플래시 화면 완성, 온보딩 화면 작업중

---
+++ HikingClub/Feature/Splash/SplashViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SplashViewController.swift
+//  HikingClub
+//
+//  Created by 이문정 on 2021/11/28.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HikingClub/Feature/Splash/SplashViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SplashViewModel.swift
+//  HikingClub
+//
+//  Created by 이문정 on 2021/11/28.
+//
+
+import Foundation
